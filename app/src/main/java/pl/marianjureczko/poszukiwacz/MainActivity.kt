01eb55648d7ead83b97cf81f37f0824a3fd2e289
mjureczko <marian.jureczko@gmail.com>
2020-01-20 18:15:58 +0100

    reading locatioon - in progress

--- app/src/main/java/pl/marianjureczko/poszukiwacz/MainActivity.kt
+++ app/src/main/java/pl/marianjureczko/poszukiwacz/MainActivity.kt
@@ -1,20 +1,29 @@
 package pl.marianjureczko.poszukiwacz
 
+import android.Manifest.permission.ACCESS_FINE_LOCATION
 import android.content.Context
+import android.content.pm.ActivityInfo
+import android.content.pm.PackageManager
 import android.location.Location
 import android.location.LocationListener
 import android.location.LocationManager
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.activity_main.*
 
+
 class MainActivity : AppCompatActivity() {
+
+    private val MY_PERMISSION_ACCESS_FINE_LOCATION = 12
 
     private var locationManager: LocationManager? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
@@ -24,27 +33,47 @@ class MainActivity : AppCompatActivity() {
         }
 
         locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        if (ContextCompat.checkSelfPermission(this, ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            locationManager!!.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000L, 1.0F, MyLocationListener())
+        } else {
+            ActivityCompat.requestPermissions(this, arrayOf(ACCESS_FINE_LOCATION),MY_PERMISSION_ACCESS_FINE_LOCATION)
+        }
 
-        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000L, 1.0F, MyLocationListener())
     }
 
+//    @Override
+//    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults)
+//    {
+//        switch(requestCode) {
+//                case MY_PERMISSION_ACCESS_FINE_LOCATION : {
+//                    if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+//                        // permission was granted
+//                    } else {
+//                        // permission denied
+//                    }
+//                    break;
+//                }
+//
+//            }
+//        }
+//    }
 }
 
 class MyLocationListener : LocationListener {
     override fun onLocationChanged(location: Location?) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        println("szerokość: ${location?.latitude} długość: ${location?.longitude}")
     }
 
     override fun onStatusChanged(provider: String?, status: Int, extras: Bundle?) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        println("onStatusChanged - provider: $provider")
     }
 
     override fun onProviderEnabled(provider: String?) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        println("onProviderEnabled - provider: $provider")
     }
 
     override fun onProviderDisabled(provider: String?) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        println("onProviderDisabled - provider: $provider")
     }
 
 }
