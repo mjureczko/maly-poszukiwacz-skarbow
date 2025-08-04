f0bdb446fea9b769a6176d1354307a259c3dec26
Marian Jureczko <marian.jureczko@gmail.com>
2025-08-04 20:36:37 +0200

    restoring tests

--- app/src/test/java/pl/marianjureczko/poszukiwacz/shared/StoragePortTest.kt
+++ app/src/test/java/pl/marianjureczko/poszukiwacz/shared/StoragePortTest.kt
@@ -3,7 +3,10 @@ package pl.marianjureczko.poszukiwacz.shared
 import android.app.Application
 import com.ocadotechnology.gembus.test.some
 import org.apache.commons.io.FileUtils
+import org.apache.commons.io.IOUtils
 import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.withinPercentage
+import org.assertj.core.api.ThrowingConsumer
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertFalse
 import org.junit.jupiter.api.Assertions.assertNull
@@ -13,9 +16,15 @@ import org.junit.jupiter.api.Test
 import pl.marianjureczko.poszukiwacz.model.Route
 import pl.marianjureczko.poszukiwacz.model.RouteArranger
 import pl.marianjureczko.poszukiwacz.model.TreasuresProgress
+import pl.marianjureczko.poszukiwacz.shared.port.storage.ExtractionProgress
 import pl.marianjureczko.poszukiwacz.shared.port.storage.StoragePort
 import pl.marianjureczko.poszukiwacz.shared.port.storage.XmlHelper
+import java.io.ByteArrayInputStream
 import java.io.File
+import java.io.StringWriter
+import java.nio.charset.StandardCharsets
+import java.util.zip.ZipEntry
+import java.util.zip.ZipInputStream
 
 class StoragePortTest {
 
@@ -164,100 +173,95 @@ class StoragePortTest {
         }
     }
 
-    //TODO: restore for classic version
-    //For apps targeting Android 14, Android prevents the Zip Path Traversal Vulnerability in the following way: ZipFile(String) and ZipInputStream.getNextEntry() throws a ZipException if zip file entry names contain ".." or start with "/".  Apps can opt-out from this validation by calling dalvik.system.ZipPathValidator.clearCallback().
-//    @Test
-//    fun `SHOULD convert route to a zip stream WHEN the route is already saved`() {
-//        //given
-//        val route = RouteArranger.savedWithTipFiles(storageHelper)
-//
-//        //when
-//        val actual = storageHelper.routeToZipOutputStream(route)
-//        val actualZip = ZipInputStream(ByteArrayInputStream(actual.toByteArray()))
-//
-//        //then
-//        var zipEntry: ZipEntry?
-//        var actualRoute: Route? = null
-//        var actualFiles: MutableSet<String> = mutableSetOf()
-//        try {
-//            while (actualZip.nextEntry.also { zipEntry = it } != null) {
-//                if (zipEntry!!.name == route.name + ".xml") {
-//                    val stringWriter = StringWriter()
-//                    IOUtils.copy(actualZip, stringWriter, StandardCharsets.UTF_8)
-//                    actualRoute = xmlHelper.loadFromString<Route>(stringWriter.toString())
-//                } else {
-//                    actualFiles.add(zipEntry!!.name)
-//                }
-//            }
-//            actualZip.close()
-//            assertRoute(actualRoute!!, route)
-//            assertRouteFiles(actualFiles, route)
-//        } finally {
-//            actualFiles.forEach {
-//                val file = File(it)
-//                if(file.exists()) {
-//                    FileUtils.delete(file)
-//                }
-//            }
-//        }
-//    }
+    @Test
+    fun `SHOULD convert route to a zip stream WHEN the route is already saved`() {
+        //given
+        val route = RouteArranger.savedWithTipFiles(storagePort)
 
-    //TODO: restore for classic version
-    //For apps targeting Android 14, Android prevents the Zip Path Traversal Vulnerability in the following way: ZipFile(String) and ZipInputStream.getNextEntry() throws a ZipException if zip file entry names contain ".." or start with "/".  Apps can opt-out from this validation by calling dalvik.system.ZipPathValidator.clearCallback().
+        //when
+        val actual = storagePort.routeToZipOutputStream(route)
+        val actualZip = ZipInputStream(ByteArrayInputStream(actual.toByteArray()))
 
-//    @Test
-//    fun `SHOULD save route with pictures and sounds WHEN loading a zip`() {
-//        //given
-//        val loggedRoutes: MutableList<String> = mutableListOf()
-//        val loggedFiles: MutableList<String> = mutableListOf()
-//        var progress: ExtractionProgress = object : ExtractionProgress {
-//            override fun routeExtracted(routeName: String) {
-//                loggedRoutes.add(routeName)
-//            }
-//
-//            override fun fileExtracted(fileName: String) {
-//                loggedFiles.add(fileName)
-//            }
-//        }
-//        val routeName = "001"
-//        val inStream = ClassLoader.getSystemResourceAsStream("route.zip")
-//        val treasure2Photo = "photo_dc4dff0e-d133-4abc-a8a5-d97bfc922f6e.jpg"
-//        val treasure2Sound = "sound_c7cb62ea-909c-4612-ba44-3083467c054d.3gp"
-//        val treasure3Photo = "photo_1d165328-7c40-486b-b802-1a84c0efd9ab.jpg"
-//        FileUtils.cleanDirectory(File(storageHelper.pathToRoutesDir()))
-//
-//        //when
-//        storageHelper.extractZipStream(inStream, progress)
-//
-//        //then
-//        val dir = storageHelper.pathToRoutesDir() + "/";
-//        var routes = storageHelper.loadAll().filter { it.name == routeName }
-//        assertThat(routes).hasSize(1)
-//        val actualRoute = routes[0]
-//        assertThat(actualRoute.treasures).hasSize(2)
-//
-//        assertThat(actualRoute.treasures).anySatisfy(ThrowingConsumer {
-//            assertThat(it.id).isEqualTo(2)
-//            assertThat(it.photoFileName).isEqualTo(dir + treasure2Photo)
-//            assertThat(it.tipFileName).isEqualTo(dir + treasure2Sound)
-//            assertThat(it.latitude).isCloseTo(51.25482, withinPercentage(0.1))
-//            assertThat(it.longitude).isCloseTo(16.9326, withinPercentage(0.1))
-//        })
-//        assertThat(actualRoute.treasures).anySatisfy(ThrowingConsumer {
-//            assertThat(it.id).isEqualTo(3)
-//            assertThat(it.photoFileName).isEqualTo(dir + treasure3Photo)
-//            assertThat(it.tipFileName).isNull()
-//            assertThat(it.latitude).isCloseTo(51.2502, withinPercentage(0.1))
-//            assertThat(it.longitude).isCloseTo(16.93156, withinPercentage(0.1))
-//        })
-//        assertThat(File("${storageHelper.pathToRoutesDir()}/$treasure2Photo").length()).isEqualTo(163850);
-//        assertThat(File("${storageHelper.pathToRoutesDir()}/$treasure2Sound").length()).isEqualTo(585221);
-//        assertThat(File("${storageHelper.pathToRoutesDir()}/$treasure3Photo").length()).isEqualTo(61781);
-//        assertThat(loggedRoutes).contains(routeName)
-//        assertThat(loggedFiles).contains(treasure2Photo)
-//        assertThat(loggedFiles).contains(treasure2Sound)
-//        assertThat(loggedFiles).contains(treasure3Photo)
-//    }
+        //then
+        var zipEntry: ZipEntry?
+        var actualRoute: Route? = null
+        var actualFiles: MutableSet<String> = mutableSetOf()
+        try {
+            while (actualZip.nextEntry.also { zipEntry = it } != null) {
+                if (zipEntry!!.name == route.name + ".xml") {
+                    val stringWriter = StringWriter()
+                    IOUtils.copy(actualZip, stringWriter, StandardCharsets.UTF_8)
+                    actualRoute = xmlHelper.loadFromString<Route>(stringWriter.toString())
+                } else {
+                    actualFiles.add(zipEntry!!.name)
+                }
+            }
+            actualZip.close()
+            assertRoute(actualRoute!!, route)
+            assertRouteFiles(actualFiles, route)
+        } finally {
+            actualFiles.forEach {
+                val file = File(it)
+                if (file.exists()) {
+                    FileUtils.delete(file)
+                }
+            }
+        }
+    }
+
+    @Test
+    fun `SHOULD save route with pictures and sounds WHEN loading a zip`() {
+        //given
+        val loggedRoutes: MutableList<String> = mutableListOf()
+        val loggedFiles: MutableList<String> = mutableListOf()
+        var progress: ExtractionProgress = object : ExtractionProgress {
+            override fun routeExtracted(routeName: String) {
+                loggedRoutes.add(routeName)
+            }
+
+            override fun fileExtracted(fileName: String) {
+                loggedFiles.add(fileName)
+            }
+        }
+        val routeName = "001"
+        val inStream = ClassLoader.getSystemResourceAsStream("route.zip")
+        val treasure2Photo = "photo_dc4dff0e-d133-4abc-a8a5-d97bfc922f6e.jpg"
+        val treasure2Sound = "sound_c7cb62ea-909c-4612-ba44-3083467c054d.3gp"
+        val treasure3Photo = "photo_1d165328-7c40-486b-b802-1a84c0efd9ab.jpg"
+        FileUtils.cleanDirectory(File(storagePort.pathToRoutesDir()))
+
+        //when
+        storagePort.extractZipStream(inStream, progress)
+
+        //then
+        val dir = storagePort.pathToRoutesDir() + "/";
+        var routes = storagePort.loadAll().filter { it.name == routeName }
+        assertThat(routes).hasSize(1)
+        val actualRoute = routes[0]
+        assertThat(actualRoute.treasures).hasSize(2)
+
+        assertThat(actualRoute.treasures).anySatisfy(ThrowingConsumer {
+            assertThat(it.id).isEqualTo(2)
+            assertThat(it.photoFileName).isEqualTo(dir + treasure2Photo)
+            assertThat(it.tipFileName).isEqualTo(dir + treasure2Sound)
+            assertThat(it.latitude).isCloseTo(51.25482, withinPercentage(0.1))
+            assertThat(it.longitude).isCloseTo(16.9326, withinPercentage(0.1))
+        })
+        assertThat(actualRoute.treasures).anySatisfy(ThrowingConsumer {
+            assertThat(it.id).isEqualTo(3)
+            assertThat(it.photoFileName).isEqualTo(dir + treasure3Photo)
+            assertThat(it.tipFileName).isNull()
+            assertThat(it.latitude).isCloseTo(51.2502, withinPercentage(0.1))
+            assertThat(it.longitude).isCloseTo(16.93156, withinPercentage(0.1))
+        })
+        assertThat(File("${storagePort.pathToRoutesDir()}/$treasure2Photo").length()).isEqualTo(163850);
+        assertThat(File("${storagePort.pathToRoutesDir()}/$treasure2Sound").length()).isEqualTo(585221);
+        assertThat(File("${storagePort.pathToRoutesDir()}/$treasure3Photo").length()).isEqualTo(61781);
+        assertThat(loggedRoutes).contains(routeName)
+        assertThat(loggedFiles).contains(treasure2Photo)
+        assertThat(loggedFiles).contains(treasure2Sound)
+        assertThat(loggedFiles).contains(treasure3Photo)
+    }
 
     @Test
     fun `SHOULD save progress to file and load it back`() {
